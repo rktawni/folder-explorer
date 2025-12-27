f96ea41f958fb486878be9dedc1f6d5ff310ae82
rktawni <ravinder.tawni@gmail.com>
2025-12-27 14:35:39 +0530

    initial code push

---
+++ src/components/nested-files/nested-file-item.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './nested-files.css';
+export default function NestedFileItem({treeItem}) {
+
+    const [item, setItem] = useState(treeItem);
+    const [showChildren, setShowChildren] = useState(false);
+
+    const setChildVisible = (evt)=>{
+        //console.log("EVENT: ", evt);
+        //evt.stopPropagation();
+        setShowChildren(!showChildren);
+    }
+    const getItem = (item)=>{
+        if(item.isFolder){
+            return(
+                <>
+                    <div>{showChildren ? <span>- </span>:<span>+ </span>}<span className='folder-item' onClick={(evt)=>setChildVisible(evt)}>{`${item.name}`}</span></div>
+                    <div className={showChildren?"child-item":"hidden-item"}>{item.children.map(node=><NestedFileItem treeItem={node}/>)}</div>
+                </>
+            )
+        } else {
+            return(
+            <div>{item.name}</div>
+            )
+        }
+    }
+    return(
+        <>
+            {getItem(item)}
+        </>
+    )
+} 
---
+++ src/components/nested-files/nested-files.jsx
@@ -0,0 +1,18 @@
+//This is a component which will demonstrate nested file structure in React
+//It'll get the data as input from the parent component and display it in a nested manner
+//In JSON, there should be keys as 'name', 'type', and 'children' (if type is folder)
+
+import { useEffect, useState } from "react";
+import NestedFileItem from './nested-file-item';
+import './nested-files.css'
+
+export default function NestedFile({ data }) {
+
+    return (
+        <div className="explorer-container">
+            {data.data.children.map(node=>
+                <NestedFileItem treeItem={node}/>
+            )}
+        </div>
+    )
+}
